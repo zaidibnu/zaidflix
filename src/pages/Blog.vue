44b499737f7af07752638dd9a9a9a109a416cb60
Zaid Ibnu Awwal <zaidibnu@gmail.com>
2023-12-23 11:52:01 +0700

    #update

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -44,9 +44,6 @@
             <h2>{{ items[current].title }}</h2>
         </div>
         <div v-html="content"></div>
-        <p
-        style="margin: 0;"
-        class="text-start p-3 g-0" v-for="i in 20" v-bind:key="i"> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
     </div>
  </div>
         <video autoplay muted loop style="width: 100vw; height: 100vh; margin-top: 0vh; object-fit: cover; position: fixed;top:0; left: 0;
@@ -79,7 +76,7 @@ export default {
   methods:{
     open(id){
         this.current = id;
-        fetch('https://raw.githubusercontent.com/zaidibnu/zaidflix/master/hosted/1.html').then((result)=>{
+        fetch(`https://raw.githubusercontent.com/zaidibnu/zaidflix/master/hosted/${this.items[id].id}.html`).then((result)=>{
             result.text().then((ctn)=>{
                 this.content =ctn;
                         this.notContent = false 
